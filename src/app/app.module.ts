6c3732f31623769a153f627599030aa35a548d2a
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2022-06-20 15:05:10 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap/alert';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BsDropdownModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
